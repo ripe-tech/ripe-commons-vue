27e1fab9a14ec377f36fa141a50a086e156ba9db
Gabriel Candal <gcc@platforme.com>
2021-02-24 14:21:51 +0000

    fix: compilation

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import { install } from "./vue";
 import { name, version } from "./package.json";
 
 export * from "./js";
@@ -6,5 +5,3 @@ export * from "./vue";
 
 export const NAME = name;
 export const VERSION = version;
-
-export default install;
